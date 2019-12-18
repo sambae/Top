a83eb8bc0f67df9c47adf3aae917425d9774e73b
Samuel Bae <me@sambae.dev>
2019-12-18 03:50:29 -0500

    Set up fed binding and viewmodel

--- app/src/main/java/com/sambae/top/feed/FeedFragment.kt
+++ app/src/main/java/com/sambae/top/feed/FeedFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.sambae.top.databinding.FragmentFeedBinding
 
 class FeedFragment: Fragment() {
@@ -14,6 +15,13 @@ class FeedFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentFeedBinding.inflate(inflater)
+        val factory = FeedViewModel.Factory()
+
+        val viewModel = ViewModelProvider(this, factory)
+            .get(FeedViewModel::class.java)
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
 
         return binding.root
     }
